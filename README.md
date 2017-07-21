eb88a4911a379d748e71d98aed78b510dcdbf417
Kirill Merkushev <lanwen@yandex.ru>
2017-07-22 01:24:20 +0300

    initial version with welcome msg

---
+++ README.md
@@ -0,0 +1,5 @@
+# Aerokubot
+
+Simple bot to help with support chat
+
+To start just pass the `--token` arg with your telegram token
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+	"flag"
+	"os"
+	"fmt"
+	"log"
+	"strings"
+)
+
+var (
+	token       string
+	version     bool
+	gitRevision string = "HEAD"
+	buildStamp  string = "unknown"
+)
+
+func init() {
+	flag.StringVar(&token, "token", "", "Telegram bot token (required)")
+	flag.BoolVar(&version, "version", false, "Show version and exit")
+	flag.Parse()
+
+
+	if len(flag.Args()) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if version {
+		showVersion()
+		os.Exit(0)
+	}
+}
+
+func showVersion() {
+	fmt.Printf("Git Revision: %s\n", gitRevision)
+	fmt.Printf("UTC Build Time: %s\n", buildStamp)
+}
+
+func showHelp() {
+	fmt.Println(`
+
+	`)
+}
+
+const welcome = `
+Welcome to Aerokube chat! We can help on english 🇬🇧, так же как и по-русски 🇷🇺!
+
+If you have trouble, provide your environment and versions for aerokube tools first!
+
+Есть проблемы? Начни вопрос с окружения и используемой версии для тулчейна aerokube!
+`
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(fmt.Errorf("[BOT_CREATION_FAIL] [%v]", err))
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, _ := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+
+		if update.Message == nil {
+			continue
+		}
+
+		if update.Message.Chat.IsGroup() {
+			if update.Message.NewChatMembers != nil {
+				newu := []string{}
+
+				for _, user := range *update.Message.NewChatMembers {
+					newu = append(newu, "@" + user.UserName)
+				}
+
+				ucall := strings.Join(newu, " ")
+
+
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Hey, %s\n%s", ucall, welcome))
+				bot.Send(msg)
+			}
+		}
+	}
+}
