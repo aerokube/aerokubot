b4483edcd98727a6cfe13cc205c0b5eea3d764c1
Kirill Merkushev <lanwen@yandex.ru>
2017-07-22 02:04:27 +0300

    upstart service

--- update.sh
+++ update.sh
@@ -3,5 +3,7 @@
 curl -Lo /opt/aerokubot/aerokubot https://github.com/aerokube/aerokubot/releases/download/$1/aerokubot && chmod +x /opt/aerokubot/aerokubot
 
 curl -Lo /etc/systemd/system/aerokubot.service https://raw.githubusercontent.com/aerokube/aerokubot/master/systemd/aerokubot.service
+curl -Lo /etc/init/aerokubot.conf https://raw.githubusercontent.com/aerokube/aerokubot/master/upstart/aerokubot.conf
 
 systemctl enable aerokubot
+service aerokubot start
